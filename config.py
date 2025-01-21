5c69bd2c8318aa6140c01be20a5bfc62fe50ffc9
dimashidyat <77518929+dimashidyat@users.noreply.github.com>
2025-01-21 17:14:05 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Token Bot Telegram
+TOKEN = "7762368836:AAFqeaOKYOtrKWOhhsu8sloF2U_wNvY2Z9I"
+
+# Chat ID buat reminder
+CHAT_ID = "2031898002"
+
+# Jadwal Harian Default
+DEFAULT_SCHEDULE = {
+    'subuh': "04:45",
+    'maxim': "07:00-10:00",
+    'study': "10:00-12:00",
+    'pempek': "21:00"
+}
